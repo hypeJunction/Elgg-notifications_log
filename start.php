d230eb3b2d5a6acd14dcce6981044ec3092e6303
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-01-30 14:11:59 +0100

    fix(log): fix logging of instant notifications

--- start.php
+++ start.php
@@ -28,39 +28,48 @@ function notifications_log_init() {
  * @return mixed
  */
 function notifications_log_sent_message($hook, $type, $return, $params) {
-	
+
 	list($hook_type, $method) = explode(':', $type);
 	if ($hook_type != 'notification') {
 		return;
 	}
 
-	$event = elgg_extract('event', $params);
 	$notification = elgg_extract('notification', $params);
 
 	$recipient = $notification->getRecipient();
 	$sender = $notification->getSender();
-
-	$action = $event->getAction();
-	$actor = $event->getActor();
-	$object = $event->getObject();
 
 	$content = json_encode(array(
 		'status' => $return === false ? 'failed' : 'sent',
 		'subject' => $notification->subject,
 		'summary' => $notification->summary,
 		'body' => $notification->body,
-		'action' => $action,
-		'object' => is_callable(array($object, 'toObject')) ? $object->toObject() : array(
-			'guid' => $object->guid,
-			'id' => $object->id,
-		),
-		'actor' => is_callable(array($actor, 'toObject')) ? $actor->toObject() : array('guid' => $event->getActorGUID()),
 	));
+	$description = 'instant';
+	
+	$event = elgg_extract('event', $params);
+	if ($event) {
+		$action = $event->getAction();
+		$actor = $event->getActor();
+		$object = $event->getObject();
+		$event_data = array(
+			'action' => $action,
+			'object' => is_callable(array($object, 'toObject')) ? $object->toObject() : array(
+				'guid' => $object->guid,
+				'id' => $object->id,
+					),
+			'actor' => is_callable(array($actor, 'toObject')) ? $actor->toObject() : array('guid' => $event->getActorGUID()),
+		);
+
+		$content = array_merge($content, $event_data);
+		$description = str_replace(':', '_', $event->getDescription());
+	}
+
 	$name = implode('_', array(
 		$method,
 		"TO-$recipient->guid",
 		"FROM-$sender->guid",
-		str_replace(':', '_', $event->getDescription()),
+		$description,
 		time(),
 	));
 
@@ -72,5 +81,4 @@ function notifications_log_sent_message($hook, $type, $return, $params) {
 
 	$filename = "{$dirname}{$name}.json";
 	file_put_contents($filename, $content);
-
 }
